aaceb97c464b6370ae13625776873f83d5feff3d
Finx <86189855+SedatTR3348237878HMLD@users.noreply.github.com>
2021-07-23 19:41:13 -0400

    Add files via upload

---
+++ Basit toolsl.sh
@@ -0,0 +1,16 @@
+cd Desktop
+touch "Yeni Metin Belgesi.txt"
+touch "Yeni Dosya"
+touch "Toolu Kullandığın İçin Eyw"
+echo -e "\033[1m \e[32m (Kanki Ben San Perl Filan Python V.b Benim Programları Filan  Kuruyom İstemiyorsan Terminali Kapat. )"
+git clone https://github.com/SedatTR3348237878HMLD/tgt-finx-termuxxtools
+git clone https://github.com/SedatTR3348237878HMLD/Termux-Veya-Linux-Guncelle
+git clone https://github.com/SedatTR3348237878HMLD/Turkish-Guyew
+apt install python -y
+apt install perl -y
+apt install python2 -y
+apt install python3 -y
+echo -e "\033[1m \e[32m (Kurulum Tamamlandı! )"
+figlet Finx | lolcat
+
+
